2de1fd4076bc70c15004f71afd9344b70c5b39f7
Michał Smyk <s.michalsmyk@gmail.com>
2018-01-26 21:26:46 +0100

    ASP.Net Core Solution & ASP.NET Core MVC project added SignalR added to the Web application, connection proof of concept implemented.

---
+++ TIR/Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Web.Models;
+using Web.Services;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHubContext<IndexHub> hub;
+
+        public HomeController(IHubContext<IndexHub> hub) => this.hub = hub;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Test()
+        {
+            hub.Clients.All.InvokeAsync("refresh");
+            return View();
+        }
+    }
+}
